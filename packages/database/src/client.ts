76f687de5e8a58ed93a5794b9f2a92234be91485
Turbobot <turbobot@vercel.com>
2024-12-09 21:04:59 +0700

    feat(create-turbo): apply official-starter transform

--- packages/database/src/client.ts
+++ packages/database/src/client.ts
@@ -1,5 +1,10 @@
 import { PrismaClient } from "@prisma/client";
 
+declare global {
+  // eslint-disable-next-line no-unused-vars
+  var prisma: PrismaClient;
+}
+
 export const prisma = global.prisma || new PrismaClient();
 
 if (process.env.NODE_ENV !== "production") global.prisma = prisma;
